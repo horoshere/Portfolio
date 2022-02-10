4ce9796836dfbb27d8c7a58b911529e17b3e2dab
Nikolay <niktyler7@gmail.com>
2022-02-10 18:04:48 +0300

    add js for progress

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,3 +8,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
   menu.classList.remove('active');
 });
+
+const persent = document.querySelectorAll('.progress__persent'),
+      lines = document.querySelectorAll('.progress__rectangle-atachment');
+
+persent.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
+});
